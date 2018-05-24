b81321818261fa1d5cd0dbace2ea405a29d6267a
FinniV <vechik.korosten@gmail.com>
2018-05-25 01:29:46 +0300

    added baseViewModel

---
+++ ANTToDo.Core/ViewModels/BaseViewModel.cs
@@ -0,0 +1,72 @@
+﻿using ANTToDo.Core.Data;
+using ANTToDo.Core.Interfaces;
+using MvvmCross.Core.Navigation;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANTToDo.Core.ViewModels
+{
+    public interface IBaseViewModel
+    {
+    }
+
+    public abstract class BaseViewModel : MvxViewModel, IBaseViewModel
+    {
+        public IRepository _repository;
+        public IMvxNavigationService _navigationService;
+
+        public BaseViewModel()
+        {
+            _repository = Mvx.Resolve<Repository>();
+            _navigationService = Mvx.Resolve<IMvxNavigationService>();
+        }
+
+        public override Task Initialize() => base.Initialize();
+        public override void Prepare() => base.Prepare();
+        public override void RaiseAllPropertiesChanged() => base.RaiseAllPropertiesChanged();
+        public override void RaisePropertyChanged(PropertyChangedEventArgs changedArgs) => base.RaisePropertyChanged(changedArgs);
+        public override void Start() => base.Start();
+        public override string ToString() => base.ToString();
+        public override void ViewAppeared() => base.ViewAppeared();
+        public override void ViewAppearing() => base.ViewAppearing();
+        public override void ViewCreated() => base.ViewCreated();
+        public override void ViewDestroy(bool viewFinishing = true) => base.ViewDestroy(viewFinishing);
+        public override void ViewDisappeared() => base.ViewDisappeared();
+        public override void ViewDisappearing() => base.ViewDisappearing();
+        protected override void InitFromBundle(IMvxBundle parameters) => base.InitFromBundle(parameters);
+        protected override MvxInpcInterceptionResult InterceptRaisePropertyChanged(PropertyChangedEventArgs changedArgs) => base.InterceptRaisePropertyChanged(changedArgs);
+        protected override void ReloadFromBundle(IMvxBundle state) => base.ReloadFromBundle(state);
+        protected override void SaveStateToBundle(IMvxBundle bundle) => base.SaveStateToBundle(bundle);
+        protected override bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null) => base.SetProperty(ref storage, value, propertyName);
+        public void Close()
+        {
+
+        }
+    }
+
+    public abstract class BaseViewModel<TParameter, TResult> : MvxViewModel<TParameter, TResult>, IBaseViewModel
+       where TParameter : class
+    {
+        protected TParameter _parameter;
+        protected IMvxNavigationService _navigationService;
+        public IRepository _repository;
+
+        public BaseViewModel(IMvxNavigationService navigationService)
+        {
+            _repository = Mvx.Resolve<Repository>();
+            _navigationService = Mvx.Resolve<IMvxNavigationService>();
+        }
+
+        public override void Prepare(TParameter parameter)
+        {
+            _parameter = parameter;
+        }
+    }
+}
